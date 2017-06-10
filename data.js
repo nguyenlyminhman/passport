fc86792c845a4a00e4fbdb46649b8f2daf73d99e
nguyenlyminhman <nguyenlyminhman@gmail.com>
2017-06-10 18:19:59 +0700

    data finish

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const data = new Sequelize({
+const db = new Sequelize({
     database: 'DEMONODE',
     username: 'postgres',
     password: 'sa',
@@ -10,9 +10,29 @@ const data = new Sequelize({
 });
 
 //test connection
-data
+db
     .authenticate()
     .then(() => { console.log('connected') })
     .catch(err => { console.log(err + '') })
 
+const dt = db.define('data',
+    {
+        name: Sequelize.STRING,
+        phone: Sequelize.INTEGER,
+        realprice: Sequelize.REAL,
+        doubleprice: Sequelize.DOUBLE,
+        status: Sequelize.BOOLEAN
+    },
+    {
+        //don't have two column in database
+        timestamps: false
+    }
+)
 
+dt.create({
+    name: 'naruto',
+    phone: 0988888,
+    realprice: 12.3,
+    doubleprice: 12.2,
+    status: true
+});
